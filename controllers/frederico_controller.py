5149ea15f70ccda06b35b52e7696b3437621dccd
samuel-cavalcanti <scavalcanti111@gmail.com>
2021-12-02 12:11:00 -0300

    substituido o nome para delta phi, o mesmo nome do angulo dito por frederico

--- controllers/frederico_controller.py
+++ controllers/frederico_controller.py
@@ -26,11 +26,12 @@ class FredericoController:
         delta_x = desired_pos.x - current.x
         delta_y = desired_pos.y - current.y
         delta_l = euclidean_distance(delta_x, delta_y)
-        delta_theta = np.arctan2(delta_y, delta_x) - current.theta_in_rads
+        phi = np.arctan2(delta_y, delta_x)
+        delta_phi =  phi - current.theta_in_rads
 
         """Controladores desacoplados"""
-        linear_velocity_in_polar_system = self.__position_controller.step(delta_l * np.cos(delta_theta))
-        angular_velocity = self.__orientation_controller.step(delta_theta)
+        linear_velocity_in_polar_system = self.__position_controller.step(delta_l * np.cos(delta_phi))
+        angular_velocity = self.__orientation_controller.step(delta_phi)
 
         """Transformando o sistema polar para cartesiano"""
         x = linear_velocity_in_polar_system * np.cos(current.theta_in_rads)
